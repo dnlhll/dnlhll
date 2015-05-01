f648e5e135338f047b8af002ff0cc8006c3acea8
Daniel Keith Hull <dnlhll@Daniels-MacBook-Pro.local>
2015-04-30 22:45:43 -0700

    update

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
 var express = require('express')
 var http = require('http')
-var path = require('path');
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
-//app.set('views', __dirname + '/');
-//app.set('view engine', 'jade');
-//app.use(express.logger('dev'));
-app.use(express.bodyParser());
-//app.use(express.methodOverride());
-//app.use(app.router);
-//app.use(express.static(path.join(__dirname, 'public')));
-if ('development' == app.get('env')) { app.use(express.errorHandler()); }
 
 app.get('/', function(req, res) { res.sendfile('index.html'); });
 
